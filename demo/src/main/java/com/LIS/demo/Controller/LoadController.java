90bc4f99bc966ce01ed1740460119c1e196f8c00
lgl <1432184038@qq.com>
2025-06-28 17:19:43 +0800

    add some js file

--- demo/src/main/java/com/LIS/demo/Controller/LoadController.java
+++ demo/src/main/java/com/LIS/demo/Controller/LoadController.java
@@ -1,19 +1,20 @@
 package com.LIS.demo.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping
+@Controller
 public class LoadController {
 
 
     @GetMapping("/load")
-    void showLoadPage(){
+    String showLoadPage(){
         //生成一个非对称密钥，将公钥交给web页面，把登陆用户密码用md5加密后，再使用非对称加密，再post
-
+        return "load";
 
     }
 
